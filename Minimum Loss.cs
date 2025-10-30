6bfa1ac2a74ea20734d4f5008a012bb9f4dfaf25
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-30 19:53:44 +0800

    Create Minimum Loss.cs

---
+++ Minimum Loss.cs
@@ -0,0 +1,71 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'minimumLoss' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY price as parameter.
+     */
+
+    public static int minimumLoss(List<long> price)
+    {
+        
+        var indexedPrices = price
+            .Select((p, i) => new { Price = p, Index = i })
+            .OrderBy(p => p.Price)
+            .ToList();
+
+        long minLoss = long.MaxValue;
+
+        
+        for (int i = 1; i < indexedPrices.Count; i++)
+        {
+            long diff = indexedPrices[i].Price - indexedPrices[i - 1].Price;
+            
+            
+            if (indexedPrices[i].Index < indexedPrices[i - 1].Index && diff < minLoss)
+            {
+                minLoss = diff;
+            }
+        }
+
+        return (int)minLoss;
+    }
+
+
+    }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> price = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(priceTemp => Convert.ToInt64(priceTemp)).ToList();
+
+        int result = Result.minimumLoss(price);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
